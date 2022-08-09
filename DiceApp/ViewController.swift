f68b9c8ddb1316f0d76a6e1d3f6dcc0d11ae84e2
Tanmay Deo <tanmay.deo@ril.com>
2022-08-09 09:44:37 +0530

    DiceApp added

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButton(_ sender: UIButton) {
+//       diceImage1.image = UIImage(named: "dice6")
+//       diceImage2.image = UIImage(named: "dice1")
+        
+        diceImage1.image = [#imageLiteral(resourceName: "dice1") , #imageLiteral(resourceName: "dice2") , #imageLiteral(resourceName: "dice3") , #imageLiteral(resourceName: "dice4") , #imageLiteral(resourceName: "dice5") , #imageLiteral(resourceName: "dice6")][Int.random(in: 0...5)]
+        diceImage2.image = [#imageLiteral(resourceName: "dice1") , #imageLiteral(resourceName: "dice2") , #imageLiteral(resourceName: "dice3") , #imageLiteral(resourceName: "dice4") , #imageLiteral(resourceName: "dice5") , #imageLiteral(resourceName: "dice6")][Int.random(in: 0...5)]
+        
     }
-
-
+    
 }
 
